41feffa6c41903797571f268c44748952265b267
hema <hemalathamadeswaran@yahoo.com>
2023-08-29 19:07:08 +0530

    form skeleton added

---
+++ src/app/amount-form/amount-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amount-form',
+  templateUrl: './amount-form.component.html',
+  styleUrls: ['./amount-form.component.css']
+})
+export class AmountFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AmountFormComponent } from './amount-form/amount-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AmountFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
